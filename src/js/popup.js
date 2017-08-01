064a96f9947268c95bbcea876e3ce2aba8a7f8f9
Emil Świniarski <emil.swiniarski7@gmail.com>
2017-08-01 23:31:41 +0200

    import and export to text file

--- src/js/popup.js
+++ src/js/popup.js
@@ -13,13 +13,64 @@ function getOpenTabsUrl() {
     return openTabs;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function exportOpenTabs() {
     var openTabs = getOpenTabsUrl();
 
     setTimeout(function() {
-        var div = document.getElementById("openTabs");
+        var fileContext = "";
+
         for (var i = 0; i < openTabs.length; i++) {
-            div.innerHTML = div.innerHTML + openTabs[i] + "<br>";
+            fileContext = fileContext + openTabs[i] + "\n";
         }
-    }, 200)
+
+        saveTextFile(fileContext, 'open-tabs.txt');
+    }, 200);
+}
+
+function saveTextFile(content, fileName) {
+    var file = new File([content], fileName, {
+        type: "text/plain;charset=utf-8"
+    });
+    saveAs(file);
+}
+
+function loadTextFile(event) {
+    var file = event.target.files[0];
+
+    if (file) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var fileLines = e.target.result.split('\n');
+
+            fileLines= fileLines.filter(function(line){
+                return line != '' ? true : false;
+            });
+            
+            openTabs(fileLines);
+        };
+
+        reader.readAsText(file);
+
+    } else {
+        console.log('Load failed');
+    }
+
+    return false;
+}
+
+function openTabs(tabs) {
+    tabs.forEach(function(tab){
+        chrome.tabs.create({
+            'url': tab
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var exportButton = document.getElementById('exportButton');
+    exportButton.addEventListener("click", exportOpenTabs, false);
+
+    var loadButton = document.getElementById('loadButton');
+    loadButton.addEventListener('change', loadTextFile, false);
 });
